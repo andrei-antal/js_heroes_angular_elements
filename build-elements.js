da1a51e82f0e1da663d3348c223bf6ba98b9f678
Andrei Antal <antal.andrei@icloud.com>
2019-02-20 10:47:53 +0200

    Update to ngx-build-plus

--- build-elements.js
+++ build-elements.js
@@ -2,22 +2,21 @@ const fs = require('fs-extra');
 const concat = require('concat');
 
 (async function build() {
-  const files = [
-    './dist/ngElem/runtime.js',
-    './dist/ngElem/polyfills.js',
-    './dist/ngElem/scripts.js',
-    './dist/ngElem/main.js'
-  ];
 
-  await fs.ensureDir('elements');
-  await concat(files, 'elements/custom-element.js');
+  await fs.ensureDir('dist');
   // HTML example
   await fs.remove('./test-html/custom-element.js')
-  await fs.copyFile('elements/custom-element.js', './test-html/custom-element.js')
+  await fs.remove('./test-html/polyfills.js')
+  await fs.copyFile('dist/ngElem/main.js', './test-html/custom-element.js')
+  await fs.copyFile('dist/ngElem/polyfills.js', './test-html/polyfills.js')
   // React example
   await fs.remove('./test-react/my-app/public/custom-element.js')
-  await fs.copyFile('elements/custom-element.js', './test-react/my-app/public/custom-element.js')
+  await fs.remove('./test-react/my-app/public/polyfills.js')
+  await fs.copyFile('dist/ngElem/main.js', './test-react/my-app/public/custom-element.js')
+  await fs.copyFile('dist/ngElem/polyfills.js', './test-react/my-app/public/polyfills.js')
   // Vue example
   await fs.remove('./test-vue/my-project/public/custom-element.js')
-  await fs.copyFile('elements/custom-element.js', './test-vue/my-project/public/custom-element.js')
+  await fs.remove('./test-vue/my-project/public/polyfills.js')
+  await fs.copyFile('dist/ngElem/main.js', './test-vue/my-project/public/custom-element.js')
+  await fs.copyFile('dist/ngElem/polyfills.js', './test-vue/my-project/public/polyfills.js')
 })();
